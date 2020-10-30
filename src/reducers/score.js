fd8d69e31eac46dd063df88db114903e33c0149c
Mark Yang <MarkX0823@gmail.com>
2020-10-30 15:33:17 +0800

    Complete score increment system and update yarn.lock

--- src/reducers/score.js
+++ src/reducers/score.js
@@ -10,17 +10,47 @@ class Player {
   }
 }
 
-class Point {
+class Score {
   constructor() {
     this.L = new Player();
     this.R = new Player();
   }
 }
 
+const incSet = winner => {
+  console.log('incSet');
+  winner.set++;
+  return;
+};
+
 const incGame = (winner, loser) => {
   console.log('incGame');
   winner.point = loser.point = 0;
   winner.ad = loser.ad = false;
+  switch (winner.game) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+      winner.game++;
+      break;
+    case 5:
+      if (loser.game < 5) {
+        incSet(winner);
+      } else if (loser.game === 5 || loser.game === 6) {
+        winner.game++;
+      } else {
+        console.log('wrong state!');
+      }
+      break;
+    case 6:
+      incSet(winner);
+      break;
+    default:
+      console.log('wrong state!');
+      break;
+  }
   return;
 };
 
@@ -35,36 +65,53 @@ const deuceCheck = (winner, loser) => {
 };
 
 const incPoint = (winner, loser) => {
-  switch (winner.point) {
-    case 0:
-    case 15:
-      winner.point += 15;
-      break;
-    case 30:
-      winner.point = 40;
-      break;
-    case 40:
-      deuceCheck(winner, loser);
-      break;
-    default:
-      break;
+  if (winner.game === 6 && loser.game === 6) {
+    console.log('tiebreak');
+    if (winner.point < 6) {
+      winner.point++;
+    } else {
+      if (loser.point < 6) {
+        incGame(winner, loser);
+      } else {
+        if (winner.point > loser.point) {
+          incGame(winner, loser);
+        } else {
+          winner.point++;
+        }
+      }
+    }
+  } else {
+    switch (winner.point) {
+      case 0:
+      case 15:
+        winner.point += 15;
+        break;
+      case 30:
+        winner.point = 40;
+        break;
+      case 40:
+        deuceCheck(winner, loser);
+        break;
+      default:
+        break;
+    }
   }
 };
 
-const calculate = (point, pos) => {
+const calculate = (score, pos) => {
   switch (pos) {
     case 'L':
-      incPoint(point.L, point.R);
+      incPoint(score.L, score.R);
       break;
     case 'R':
-      incPoint(point.R, point.L);
+      incPoint(score.R, score.L);
       break;
     default:
       break;
   }
 };
 
-const score = (state = [new Point()], action) => {
+const score = (state = [new Score()], action) => {
   switch (action.type) {
     case 'INCREASE_SCORE':
       const point = cloneDeep(state[state.length - 1]);
